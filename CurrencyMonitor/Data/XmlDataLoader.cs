04d9081985b006010863248b95e821b1239c58c7
Felipe Aburaya <felipe.aburaya@hotmail.com>
2020-12-27 16:56:31 +0100

    Datenbank wird während erster Initialisierung ausgefüllt

---
+++ CurrencyMonitor/Data/XmlDataLoader.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CurrencyMonitor.Data
+{
+    /// <summary>
+    /// Lädt Daten aus einer XML datei und speichert sie.
+    /// </summary>
+    public class XmlDataLoader
+    {
+        public enum File { InitialData }
+
+        private static XmlDocument ParseXmlFile(File file)
+        {
+            var dom = new XmlDocument();
+
+            switch (file)
+            {
+                case File.InitialData:
+                    dom.Load(System.IO.Path.Combine("Data", "InitializationData.xml"));
+                    break;
+
+                default:
+                    throw new NotSupportedException($"XML-Datei {file} ist nicht zum Laden unterstützt!");
+            }
+
+            return dom;
+        }
+
+
+        private XmlDocument _dom;
+        private Func<XmlDocument> _delayedParseXml;
+        private XmlDocument XmlDataSource
+        {
+            get { return _dom ??= _delayedParseXml(); }
+        }
+
+        public XmlDataLoader(File file)
+        {
+            _delayedParseXml = (() => ParseXmlFile(file));
+        }
+
+        public enum DataSet { Currencies }
+
+        /// <summary>
+        /// Lädt die Daten aus der XML-Datei und speichert sie durch EF.
+        /// </summary>
+        /// <param name="dataSet">Welche Daten geladen werden müssen.</param>
+        /// <param name="dbContext">Die Datenkontextklasse, wo die Daten gespeichert werden.</param>
+        public void Load(DataSet dataSet, IServiceProvider serviceProvider)
+        {
+            using CurrencyMonitorContext dbContext = new CurrencyMonitorContext(
+                serviceProvider.GetRequiredService<DbContextOptions<CurrencyMonitorContext>>());
+
+            switch (dataSet)
+            {
+                case DataSet.Currencies:
+                    LoadCurrencies(dbContext.RecognizedCurrency);
+                    break;
+
+                default:
+                    throw new NotSupportedException($"DataSet {dataSet} ist nicht zum Laden unterstützt!");
+            }
+
+            dbContext.SaveChanges();
+        }
+
+        private void LoadCurrencies(DbSet<DataModels.RecognizedCurrency> intoDataSet)
+        {
+            if (intoDataSet.Any())
+                return;
+
+            const string xpath = "/deployment/currencies/entry";
+            foreach (XmlNode node in XmlDataSource.SelectNodes(xpath))
+            {
+                var entry = node as XmlElement;
+
+                var deserializedObject = new DataModels.RecognizedCurrency
+                {
+                    Code = entry.GetAttribute("code"),
+                    Name = entry.GetAttribute("name"),
+                    Symbol = entry.GetAttribute("symbol"),
+                    Country = entry.GetAttribute("country")
+                };
+
+                intoDataSet.Add(deserializedObject);
+            }
+        }
+
+    } // end of class XmlDataLoader
+}// end of namespace CurrencyMonitor.Data
--- CurrencyMonitor/Program.cs
+++ CurrencyMonitor/Program.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -14,7 +14,25 @@ namespace CurrencyMonitor
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+
+                try
+                {
+                    var xmlDataLoader = new Data.XmlDataLoader(Data.XmlDataLoader.File.InitialData);
+                    xmlDataLoader.Load(Data.XmlDataLoader.DataSet.Currencies, serviceProvider);
+                }
+                catch (Exception ex)
+                {
+                    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Während der ersten Ausfüllung der Datenbank ist ein Felher aufgetreten!");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
