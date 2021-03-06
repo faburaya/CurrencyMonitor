f2d045343b9f11ff2ac649f8908b7d865a84d8f3
Felipe Aburaya <felipe.aburaya@hotmail.com>
2021-03-06 10:50:23 -0300

    Neue Klasse: Leser des Wechselkurses in der Webseite

---
+++ CurrencyMonitor.DataAccess.UnitTests/ExchangeRateReaderTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Xunit;
+
+namespace CurrencyMonitor.DataAccess.UnitTests
+{
+    public class ExchangeRateReaderTest
+    {
+        [Fact]
+        public void Constructor_WhenCodesValid()
+        {
+            new ExchangeRateReader("EUR", "BRL");
+        }
+
+        [Fact]
+        public void Constructor_WhenCodesInvalid_ThenThrow()
+        {
+            Assert.Throws<ArgumentException>(() => new ExchangeRateReader("EUR", "Ungültiges"));
+            Assert.Throws<ArgumentException>(() => new ExchangeRateReader("Ungültiges", "BRL"));
+        }
+
+        [Fact]
+        public void TryRead_WhenNotCompliant_ThenGiveZeroAndFalse()
+        {
+            double rate;
+            var reader = new ExchangeRateReader("EUR", "BRL");
+            Assert.False(reader.TryRead("<div>Keine nützliche Daten</div>", out rate));
+            Assert.Equal(0.0, rate);
+        }
+
+        [Fact]
+        public void TryRead_WhenCompliant_ThenGiveRateAndTrue()
+        {
+            double rate;
+            var reader = new ExchangeRateReader("EUR", "BRL");
+            Assert.True(
+                reader.TryRead(@"
+                    <div class=""cc-rate"">
+                        <label title=""Exchange Rate"" id=""elb"" accesskey=""x"" for=""cc-ratebox"">Umrechnungskurs (Kauf/Verkauf)</label>
+                        <div tabindex=""4"" aria-labelledby=""elb"" id=""cc-ratebox"">BRL/EUR = 6,7805</div>
+                    </div>", out rate));
+
+            Assert.Equal(6.7805, rate);
+        }
+    }
+}
---
+++ CurrencyMonitor.DataAccess/ExchangeRateReader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CurrencyMonitor.DataAccess
+{
+    /// <summary>
+    /// Liest den Wert des Wechselkurses aus dem HTML-Text einer Webseite.
+    /// </summary>
+    /// <remarks>Diese implementierung setzt auf https://themoneyconverter.com/ </remarks>
+    public class ExchangeRateReader
+    {
+        private readonly static Regex currencyCodeRegex;
+
+        static ExchangeRateReader()
+        {
+            currencyCodeRegex = new Regex("^[A-Z]{3}$", RegexOptions.Compiled);
+        }
+
+        private void ValidateCurrencyCode(string currencyCode)
+        {
+            if (!currencyCodeRegex.IsMatch(currencyCode))
+            {
+                throw new ArgumentException($"{currencyCode} is kein gültiges Code für eine Währung!");
+            }
+        }
+
+        private readonly Regex _exchangeRateRegex;
+
+        public ExchangeRateReader(string sellingCurrencyCode, string buyingCurrencyCode)
+        {
+            ValidateCurrencyCode(sellingCurrencyCode);
+            ValidateCurrencyCode(buyingCurrencyCode);
+
+            _exchangeRateRegex =
+                new Regex($@"{buyingCurrencyCode}/{sellingCurrencyCode} = (\d+,\d+)", RegexOptions.Compiled);
+        }
+
+        /// <summary>
+        /// Versucht, dem Hypertext den Wechselkurs zu entnehmen.
+        /// </summary>
+        /// <param name="hyperText">Der zu lesende HTML-Inhalt.</param>
+        /// <param name="exchangeRate">Wird den Wechselkurs zurückgewiesen.</param>
+        /// <returns>Ob das Lesen von dem Wert des Wechselkurses gelungen ist.</returns>
+        public bool TryRead(string hyperText, out double exchangeRate)
+        {
+            exchangeRate = 0.0;
+
+            var match = _exchangeRateRegex.Match(hyperText);
+            if (match.Success)
+            {
+                return double.TryParse(match.Groups[1].Value, out exchangeRate);
+            }
+
+            return false;
+        }
+
+    }// end of class ExchangeRateReader
+
+}// end of namespace CurrencyMonitor.DataAccess
