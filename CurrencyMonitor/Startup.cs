17c74b7350af0eaa0bbb3b764217c62f47d8df0e
Felipe Aburaya <felipe.aburaya@hotmail.com>
2021-03-29 18:45:47 -0300

    Bescheunigt das Hochfahren der Webseite

--- CurrencyMonitor/Startup.cs
+++ CurrencyMonitor/Startup.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.IO;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,18 +30,16 @@ namespace CurrencyMonitor
         /// </summary>
         /// <typeparam name="ItemType">Der Typ des Elements, den der Service behandelt.</typeparam>
         /// <param name="services">Die Sammlung, in der der Service injiziert wird.</param>
-        private void InjectCosmosDbService<ItemType>(IServiceCollection services)
+        private async Task InjectCosmosDbService<ItemType>(IServiceCollection services)
             where ItemType : CosmosDbItem<ItemType>, IEquatable<ItemType>
         {
             string databaseName = _configuration.GetSection("CosmosDb").GetSection("DatabaseName").Value;
             string connectionString = _connStringProvider.GetSecretConnectionString("CurrencyMonitorCosmos");
 
-            services.AddSingleton<ICosmosDbService<ItemType>>(
-                CosmosDbService<ItemType>
-                    .InitializeCosmosClientInstanceAsync(databaseName, connectionString)
-                    .GetAwaiter()
-                    .GetResult()
-            );
+            var service = await CosmosDbService<ItemType>
+                .InitializeCosmosClientInstanceAsync(databaseName, connectionString);
+
+            services.AddSingleton<ICosmosDbService<ItemType>>(service);
         }
 
         /// <summary>
@@ -53,9 +50,11 @@ namespace CurrencyMonitor
         {
             services.AddControllersWithViews();
 
-            InjectCosmosDbService<DataModels.RecognizedCurrency>(services);
-            InjectCosmosDbService<DataModels.SubscriptionForExchangeRate>(services);
-            InjectCosmosDbService<DataModels.ExchangeRate>(services);
+            Task.WaitAll(new Task[] {
+                InjectCosmosDbService<DataModels.RecognizedCurrency>(services),
+                InjectCosmosDbService<DataModels.SubscriptionForExchangeRate>(services),
+                InjectCosmosDbService<DataModels.ExchangeRate>(services)
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
