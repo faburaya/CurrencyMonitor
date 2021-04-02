aa1fdcb419a298de4cc94c91557e6d42047e0230
Felipe Aburaya <felipe.aburaya@hotmail.com>
2021-04-01 21:34:00 -0300

    Grundlegende Implementierung für die Bechachrichtigung der Abonnenten

---
+++ CurrencyMonitor.ExchangeRateNotifierJob/ISubscriberNotifier.cs
@@ -0,0 +1,17 @@
+﻿using CurrencyMonitor.DataModels;
+
+namespace CurrencyMonitor.ExchangeRateNotifierJob
+{
+    /// <summary>
+    /// Schnittstelle für die Benachrichtigung der Abonnenten.
+    /// </summary>
+    public interface ISubscriberNotifier
+    {
+        /// <summary>
+        /// Benachrichtigt einen Abonnenten, der den Wechselkurs beobachtet.
+        /// </summary>
+        /// <param name="subscription">Das Abonnement.</param>
+        /// <param name="exchangeRate">Der beobachtende Wechselkurs.</param>
+        void Notify(SubscriptionForExchangeRate subscription, ExchangeRate exchangeRate);
+    }
+}
---
+++ CurrencyMonitor.ExchangeRateNotifierJob/SubscriberMailer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Mail;
+
+using CurrencyMonitor.DataModels;
+using Reusable.Utils;
+
+namespace CurrencyMonitor.ExchangeRateNotifierJob
+{
+    /// <summary>
+    /// Benachrichtigt den Abonnenten per E-Mail.
+    /// </summary>
+    public class SubscriberMailer : ISubscriberNotifier
+    {
+        private readonly SmtpClient _smtpClient;
+
+        private readonly string _senderEmail;
+
+        /// <summary>
+        /// Erstellt ein neues Objekt.
+        /// </summary>
+        /// <param name="relaySmtpServer">URL des Servers.</param>
+        /// <param name="senderEmail">E-Mail-Adresse des Senders.</param>
+        public SubscriberMailer(string relaySmtpServer,
+                                string senderEmail,
+                                PasswordBasedCredential credential)
+        {
+            _senderEmail = senderEmail;
+            _smtpClient = new SmtpClient(relaySmtpServer)
+            {
+                UseDefaultCredentials = false,
+                Credentials = new System.Net.NetworkCredential(credential.UserId, credential.Password)
+            };
+        }
+
+        public void Notify(SubscriptionForExchangeRate subscription, ExchangeRate exchangeRate)
+        {
+            if (exchangeRate.PriceOfPrimaryCurrency < 1.0)
+            {
+                exchangeRate = exchangeRate.Revert();
+            }
+
+            var message = new MailMessage(
+                _senderEmail,
+                subscription.EMailAddress,
+                $"Wechselkurs {subscription.CodeCurrencyToSell}->{subscription.CodeCurrencyToBuy} hat den gewünschten Wert erreicht",
+                $"1 {exchangeRate.PrimaryCurrencyCode} = {exchangeRate.PriceOfPrimaryCurrency} {exchangeRate.SecondaryCurrencyCode} [{exchangeRate.Timestamp}]\n");
+
+            _smtpClient.Send(message);
+        }
+
+    }// end of class SubscriberMailer
+
+}// end of namespace CurrencyMonitor.ExchangeRateNotifierJob
