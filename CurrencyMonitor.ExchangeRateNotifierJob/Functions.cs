a46350e8fd4505eea93fb2dc2c9400f35cf1d135
Felipe Aburaya <felipe.aburaya@hotmail.com>
2021-04-02 08:37:46 -0300

    Überarbeiten der Funktion für Benachrichtigung

--- CurrencyMonitor.ExchangeRateNotifierJob/Functions.cs
+++ CurrencyMonitor.ExchangeRateNotifierJob/Functions.cs
@@ -1,29 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-using CurrencyMonitor.DataModels;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Reusable.DataAccess;
 using Reusable.Utils;
+using CurrencyMonitor.DataModels;
 
 namespace CurrencyMonitor.ExchangeRateNotifierJob
 {
     /// <summary>
     /// Veröffentlicht Funktionen, die von dem Webauftrag ausgeführt werden müssen.
     /// </summary>
-    public class Functions
+    public class Functions : NotificationLogic
     {
-        private readonly ICosmosDbService<SubscriptionForExchangeRate> _subscriptionService;
-
         public Functions(ICosmosDbService<SubscriptionForExchangeRate> subscriptionService)
+            : base(subscriptionService)
         {
-            _subscriptionService = subscriptionService;
         }
 
         private const int _maxParallelTasks = 40;
@@ -32,8 +26,9 @@ namespace CurrencyMonitor.ExchangeRateNotifierJob
         /// Läuft jedes Mal wenn ein Wechselkurs sich in der Datenbank ändert
         /// und gibt den Abonnenten Bescheid, wenn es notwendig ist.
         /// </summary>
-        /// <param name="input">Eine Liste von Dokumenten, die den Wechselkursen entsprechen,
-        /// die sich änderten.</param>
+        /// <param name="input">
+        /// Eine Liste von Dokumenten, die den veränderten Wechselkursen entsprechen.
+        /// </param>
         /// <param name="log">Gewährt Protokollierung.</param>
         public void Run([CosmosDBTrigger(
             databaseName: "CurrencyMonitorDatenbank",
@@ -56,63 +51,9 @@ namespace CurrencyMonitor.ExchangeRateNotifierJob
                 ExchangeRate exchangeRate =
                     JsonConvert.DeserializeObject<ExchangeRate>(document.ToString());
 
-                // welche Abonnements werden betroffen?
-                Task task = _subscriptionService.QueryAsync(source =>
-                    source.Where(item =>
-                        item.CodeCurrencyToBuy == exchangeRate.PrimaryCurrencyCode
-                         || item.CodeCurrencyToBuy == exchangeRate.SecondaryCurrencyCode)
-                    .Select(item => item)
-                ).ContinueWith(antecendent => {
-                    IEnumerable<SubscriptionForExchangeRate> subscriptions = antecendent.Result;
-                    var subscriptionsToNotify = from item in subscriptions
-                                                where NeedsNotification(item, exchangeRate)
-                                                select item;
-
-                    foreach (SubscriptionForExchangeRate subscription in subscriptionsToNotify)
-                    {
-                        log.LogInformation($"Muss {subscription.EMailAddress} Bescheid geben.");
-                    }
-                });
-
-                parallelTasks.Add(task);
+                parallelTasks.Add(
+                    VerifyExchangeRateAgainstSubscriptionsAndNotify(exchangeRate, log));
             }
-        }
-
-        /// <summary>
-        /// Erkennt, wenn einem Abonnent wegen der Änderung des Wechselkurses
-        /// Bescheid gegeben werden muss.
-        /// </summary>
-        /// <param name="subscription">Das Abonnenment.</param>
-        /// <param name="exchangeRate">Der Wechselkurs.</param>
-        /// <returns>Ob dem Abonnent Bescheid gegeben werden muss.</returns>
-        private static bool NeedsNotification(SubscriptionForExchangeRate subscription,
-                                              ExchangeRate exchangeRate)
-        {
-            if (subscription.CodeCurrencyToBuy == exchangeRate.PrimaryCurrencyCode
-                && subscription.CodeCurrencyToSell == exchangeRate.SecondaryCurrencyCode)
-            {
-                decimal actualPriceToPay = new decimal(exchangeRate.PriceOfPrimaryCurrency);
-                decimal targetPriceToPay = subscription.TargetPriceOfSellingCurrency;
-                return actualPriceToPay <= targetPriceToPay
-                    && AreDifferentDays(exchangeRate.Timestamp, subscription.LastNotification);
-            }
-            else if (subscription.CodeCurrencyToSell == exchangeRate.PrimaryCurrencyCode
-                  && subscription.CodeCurrencyToBuy == exchangeRate.SecondaryCurrencyCode)
-            {
-                decimal actualSellingValue = new decimal(exchangeRate.PriceOfPrimaryCurrency);
-                decimal targetSellingValue = subscription.TargetPriceOfSellingCurrency;
-                return actualSellingValue >= targetSellingValue
-                    && AreDifferentDays(exchangeRate.Timestamp, subscription.LastNotification);
-            }
-            else
-            {
-                throw new ArgumentException($"Abonnement {subscription.Id} für {subscription.CodeCurrencyToSell}-> {subscription.CodeCurrencyToBuy} und Wechselkurs {exchangeRate.PrimaryCurrencyCode}<->{exchangeRate.SecondaryCurrencyCode} stimmen nicht überein!");
-            }
-        }
-
-        private static bool AreDifferentDays(DateTime a, DateTime b)
-        {
-            return a.Year != b.Year || a.DayOfYear != b.DayOfYear;
         }
 
     }// end of class Functions
---
+++ CurrencyMonitor.ExchangeRateNotifierJob/NotificationLogic.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+using Reusable.DataAccess;
+using CurrencyMonitor.DataModels;
+
+namespace CurrencyMonitor.ExchangeRateNotifierJob
+{
+    /// <summary>
+    /// Implementiert die Logik für Benachrichtigung der Abonnenten.
+    /// </summary>
+    public class NotificationLogic
+    {
+        private readonly ICosmosDbService<SubscriptionForExchangeRate> _subscriptionService;
+
+        public NotificationLogic(ICosmosDbService<SubscriptionForExchangeRate> subscriptionService)
+        {
+            _subscriptionService = subscriptionService;
+        }
+
+        /// <summary>
+        /// Überprüft welche Abonnements werden von der Änderung des gegebenen Wechselkurses
+        /// betroffen und benachrichtigt die Abonnenten.
+        /// </summary>
+        /// <param name="exchangeRate">Der veränderte Wechselkurs.</param>
+        /// <param name="log">Gewährt Protokollierung.</param>
+        /// <returns>Diese Aufgabe, die asynchron läuft.</returns>
+        public Task VerifyExchangeRateAgainstSubscriptionsAndNotify(ExchangeRate exchangeRate, ILogger log)
+        {
+            return _subscriptionService.QueryAsync(source =>
+                source.Where(item =>
+                    item.CodeCurrencyToBuy == exchangeRate.PrimaryCurrencyCode
+                        || item.CodeCurrencyToBuy == exchangeRate.SecondaryCurrencyCode)
+                .Select(item => item)
+            ).ContinueWith(antecendent =>
+            {
+                IEnumerable<SubscriptionForExchangeRate> subscriptions = antecendent.Result;
+                var subscriptionsToNotify = from item in subscriptions
+                                            where NeedsNotification(item, exchangeRate)
+                                            select item;
+
+                foreach (SubscriptionForExchangeRate subscription in subscriptionsToNotify)
+                {
+                    log.LogInformation($"Muss {subscription.EMailAddress} Bescheid geben.");
+                }
+            });
+        }
+
+        /// <summary>
+        /// Erkennt, wenn einem Abonnent wegen der Änderung des Wechselkurses
+        /// Bescheid gegeben werden muss.
+        /// </summary>
+        /// <param name="subscription">Das Abonnenment.</param>
+        /// <param name="exchangeRate">Der Wechselkurs.</param>
+        /// <returns>Ob dem Abonnent Bescheid gegeben werden muss.</returns>
+        private static bool NeedsNotification(SubscriptionForExchangeRate subscription,
+                                              ExchangeRate exchangeRate)
+        {
+            if (subscription.CodeCurrencyToBuy == exchangeRate.PrimaryCurrencyCode
+                && subscription.CodeCurrencyToSell == exchangeRate.SecondaryCurrencyCode)
+            {
+                decimal actualPriceToPay = new decimal(exchangeRate.PriceOfPrimaryCurrency);
+                decimal targetPriceToPay = subscription.TargetPriceOfSellingCurrency;
+                return actualPriceToPay <= targetPriceToPay
+                    && AreDifferentDays(exchangeRate.Timestamp, subscription.LastNotification);
+            }
+            else if (subscription.CodeCurrencyToSell == exchangeRate.PrimaryCurrencyCode
+                  && subscription.CodeCurrencyToBuy == exchangeRate.SecondaryCurrencyCode)
+            {
+                decimal actualSellingValue = new decimal(exchangeRate.PriceOfPrimaryCurrency);
+                decimal targetSellingValue = subscription.TargetPriceOfSellingCurrency;
+                return actualSellingValue >= targetSellingValue
+                    && AreDifferentDays(exchangeRate.Timestamp, subscription.LastNotification);
+            }
+            else
+            {
+                throw new ArgumentException($"Abonnement {subscription.Id} für {subscription.CodeCurrencyToSell}-> {subscription.CodeCurrencyToBuy} und Wechselkurs {exchangeRate.PrimaryCurrencyCode}<->{exchangeRate.SecondaryCurrencyCode} stimmen nicht überein!");
+            }
+        }
+
+        private static bool AreDifferentDays(DateTime a, DateTime b)
+        {
+            return a.Year != b.Year || a.DayOfYear != b.DayOfYear;
+        }
+
+    }// end of class NotificationLogic
+
+}
